2db202a8acd5bf28693b23baccfcc76fa15eb0c1
Shantay Grady <shantaygrady@gmail.com>
2020-05-03 23:10:01 -0400

    updates Bubles

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -36,7 +36,7 @@ const Login = props => {
       <p>Please enter your account information below!</p>
       <form onSubmit={handleSubmit}>
         <label htmlFor="username">Username:</label>
-        <input type="text" name="username" placeholder="Lambda School" value={credentials.password} onChange={handleChange} />
+        <input type="text" name="username" placeholder="Lambda School" value={credentials.username} onChange={handleChange} />
         <label htmlFor="password">Password:</label>
         <input type="password" name="password" placeholder="password" value={credentials.password} onChange={handleChange}  />
         <button type="submit">Login</button>
