061fa3c12855b65858bd04b3699c34888fa19e7c
Shantay Grady <shantaygrady@gmail.com>
2020-05-03 22:03:53 -0400

    create utils folder and PrivateRoute

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import PrivateRoute from "./utils/PrivateRoute.js";
-import BubblePage from "./components/BubblePage.js"
+
 
 import Login from "./components/Login";
 import "./styles.scss";
@@ -10,9 +10,7 @@ function App() {
   const [colorList, setColorList] = useState([]);
   return (
     <Router>
-      <div className="App">
-        
-      </div>
+      <PrivateRoute path="/bubbles" component={BubblePage} />
     </Router>
   );
 }
